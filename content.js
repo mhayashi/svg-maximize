81cb2a1568d91a68929d35e5ab4d4b552a68b854
Masahiro Hayashi <hayashi.masahiro@gmail.com>
2023-05-30 16:06:46 +0900

    make it work only with mermaid

--- content.js
+++ content.js
@@ -4,22 +4,25 @@ document.addEventListener('click', function(event) {
   if (target.tagName === 'svg') {
     // SVG要素がクリックされた場合の処理を行います
     const container = target.parentElement;
+    const isMermaid = container.classList.contains('mermaid');
     const isMaximized = container.classList.contains('maximized');
 
-    if (isMaximized) {
-      // 最大化解除の処理を実行します
-      container.classList.remove('maximized');
-      container.removeAttribute('style');
-    } else {
-      // 最大化表示の処理を実行します
-      container.classList.add('maximized');
-      container.style.width = '100%';
-      container.style.height = '100%';
-      container.style.position = 'fixed';
-      container.style.top = '0';
-      container.style.left = '0';
-      container.style.zIndex = '9999';
-      container.style.background = '#fff';
+    if (isMermaid) {
+      if (isMaximized) {
+        // 最大化解除の処理を実行します
+        container.classList.remove('maximized');
+        container.removeAttribute('style');
+      } else {
+        // 最大化表示の処理を実行します
+        container.classList.add('maximized');
+        container.style.width = '100%';
+        container.style.height = '100%';
+        container.style.position = 'fixed';
+        container.style.top = '0';
+        container.style.left = '0';
+        container.style.zIndex = '9999';
+        container.style.background = '#fff';
+      }
     }
   }
 });
