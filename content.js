8f80d719065d8da0814d2b740c23c20bb18c5d98
Masahiro Hayashi <hayashi.masahiro@gmail.com>
2023-08-14 14:23:11 +0900

    zoom in

--- content.js
+++ content.js
@@ -5,16 +5,13 @@ document.addEventListener('click', function(event) {
     // SVG要素がクリックされた場合の処理を行います
     const container = target.parentElement;
     const isMermaid = container.classList.contains('mermaid');
-    const isMaximized = container.classList.contains('maximized');
-
+    let state = container.getAttribute('data-state');
+    
     if (isMermaid) {
-      if (isMaximized) {
-        // 最大化解除の処理を実行します
-        container.classList.remove('maximized');
-        container.removeAttribute('style');
-      } else {
-        // 最大化表示の処理を実行します
-        container.classList.add('maximized');
+      if (!state) {
+        // 初回クリック時の処理（最大化1）
+        state = 'maximized1';
+        container.setAttribute('data-state', state);
         container.style.width = '100%';
         container.style.height = '100%';
         container.style.position = 'fixed';
@@ -22,6 +19,23 @@ document.addEventListener('click', function(event) {
         container.style.left = '0';
         container.style.zIndex = '9999';
         container.style.background = '#fff';
+      } else if (state === 'maximized1') {
+        // 2回目のクリック時の処理（最大化2）
+        state = 'maximized2';
+        container.setAttribute('data-state', state);
+        
+        // マウスの位置を取得
+        const mouseX = event.clientX;
+        const mouseY = event.clientY;
+        
+        // 拡大表示する位置を調整
+        container.style.transform = 'scale(2)';
+        container.style.transformOrigin = `${mouseX}px ${mouseY}px`;
+      } else {
+        // 3回目のクリック時の処理（元のサイズに戻す）
+        state = '';
+        container.removeAttribute('data-state');
+        container.removeAttribute('style');
       }
     }
   }
